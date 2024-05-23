a17c0b4f614efd67da2b2a78561e1e7096c743be
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-24 02:25:44 +0300

    first try

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,1 +1,54 @@
 #!/bin/bash
+
+#check path to key
+if [ -z "$KEY_PATH" ]; then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+
+# check if ip add are given
+if [ "$#" -eq 0 ]; then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
+
+
+# Check if 1 addres are given
+if [ "$#" -eq 1 ]; then
+    # "JUMP TO PUBLIC SERVER"
+
+ # Assign command-line arguments to variables
+    jump_host="$1"
+    user="ubuntu"
+
+ # SSH command with jump host and specified private key
+    ssh -i $KEY_PATH $user@$jump_host
+
+fi
+
+# Check if correct number of arguments are provided
+if [ "$#" -eq 2 ]; then
+    # "JUMP TO privte SERVER"
+
+ # Assign command-line arguments to variables
+    jump_host="$1"
+    privte_host="$2"
+    user="ubuntu"
+
+    ssh -i $KEY_PATH -t $user@$jump_host "ssh -i keypair $user@$privte_host -t"
+
+fi
+
+# Check if correct number of arguments are provided
+if [ "$#" -eq 3 ]; then
+    # "JUMP TO privte SERVER and send command"
+
+ # Assign command-line arguments to variables
+    jump_host="$1"
+    privte_host="$2"
+    user="ubuntu"
+    command="$3"
+
+    ssh -i $KEY_PATH -t $user@$jump_host "ssh -i keypair $user@$privte_host -t" "$command"
+
+fi
--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+# Function to generate a new SSH key pair
+generate_ssh_key() {
+    ssh-keygen -t rsa -b 4096 -N "" -f "/home/ubuntu/keypairtorotate"
+}
+
+if [ "$#" -eq 0 ]; then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
+
+# Check if 1 addres are given
+if [ "$#" -eq 1 ]; then
+    generate_ssh_key
+    # copy new key to ssh/authorized_key to private and delete the old one
+    cat keypairtorotate.pub | ssh -i keypair ubuntu@$1 'cat > .ssh/authorized_keys'
+    #save the new key at keypair file
+    chmod +w keypair
+    cat keypairtorotate > keypair
+    chmod 400 keypair
+    rm keypairtorotate keypairtorotate.pub
+
+fi
